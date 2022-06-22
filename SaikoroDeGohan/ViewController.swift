5ae5adce5aed6543023385561231498eca2857fb
金田岳大 <kanedtakahiro@kanedatakeshidainoMacBook-Air.local>
2022-06-22 17:04:31 +0900

    新規プロジェクト作成

---
+++ SaikoroDeGohan/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SaikoroDeGohan
+//
+//  Created by 金田岳大 on 2022/06/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
